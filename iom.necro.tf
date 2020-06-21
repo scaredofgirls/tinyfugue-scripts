ce5217818f6dd15ced7c2c1b2425b14776fe91b3
Kap <github@scaredofgirls.net>
2020-06-21 17:44:10 -0400

    Adding yeti to prots.ini for IoM

--- iom.necro.tf
+++ iom.necro.tf
@@ -7,43 +7,60 @@
 /def key_f4=!cast combine corpses
 /def key_f5=!cast blister skin
 /def key_f6=!cast mind blast
-/def key_f7=!do vc at a x;nk at a x
+; Make this a macro TODO
+/def key_f7=/nk at a x
 /def key_f8=!cast rotting limbs
 /def key_f9=!cast energy field
 /def key_f10=!cast speak with priest
 /def key_f11=!cast speak with magi
-/def key_f12=!use repair undead at nessy
+/def key_f12=!use repair undead at %{abom_name}
 ;Other Keys
-/def -b'^[1' = !cast mind surge at nessy
-/def -b'^[2' = !cast mind surge at tp
-/def -b'^[3' = !cast mind surge at korfula
-/def -b'^[4' = !cast mind surge at %{zombie_name}
-/def -b'^[5' = !cast mind surge at wilbur
+/def -b'^[1' = !cast mind surge at %{abom_name}
+/def -b'^[2' = !cast mind surge at %{mummy_name}
+/def -b'^[3' = !cast mind surge at %{vampire_name}
+/def -b'^[4' = !cast mind surge at %{wraith_name}
+/def -b'^[5' = !cast mind surge at %{zombie_name}
 /def -b'^h' = !cast cause light wounds with %{healtarg}
 
+;Helpful info during reinc/training
+/def gems = /echo "Bone Mage: Blue, Voodooist: Red, Necroscope: White, Dreadlord: Orange, Scourge: Yellow, Stygian Sorcerer: Green, Embalmer: Purple, Blood Brother: Black, Lords of Undeath: Gold"
+;Summon familiars
+;Uses spell queue, so you'll need to /set squeue 1 for spells after the first to cast
+/def nsum = /qs cast recall minion with mummy%;/qs cast recall minion with vampire%;/qs cast recall minion with wraith%;/qs cast recall minion with zombie%;cast recall minion with abomination
+
+;Attack macros
+; nk: "necro kill"
+/def nk = do cast voodoo curse %*;order %{zombie_name} use encephalitis %*;order %{abom_name} use bash %*;order %{wraith_name} cast mind intrusion %*;order %{mummy_name} use akh ba attack %*;order %{vampire_name} use kiss of life %*
+;/def nk = do order %{abom_name} use bash %*;order %{wraith_name} cast ethereal fist %*
 ;Familiar stuffs
-/def -p1 -mglob -t"Your familiar's name is now Abomination." name_nessy = /repeat -5 1 /eval !do order abomination follow;order abomination name Nessy;order abomination tank;order abomination autoreport;order abomination wield all;order abomination setwimpy 20
+/def -p1 -mglob -t"Your familiar's name is now Abomination." name_abom = /repeat -5 1 /eval !do order abomination follow;order abomination name %{abom_name};order abomination tank;order abomination autoreport;order abomination wield all;order abomination setwimpy 20
 /def -p1 -mglob -t"Your familiar's name is now Zombie." name_zombie = /repeat -5 1 /eval !do order zombie follow;order zombie name %{zombie_name};order zombie setwimpy 30;order zombie wear all
-/def -p1 -mglob -t"Your familiar's name is now Wraith." name_wilbur = /repeat -5 1 /eval !do order wraith name Wilbur;order wraith follow;order wraith setwimpy 30;order wraith wear all;order wraith wield all
-/def -p1 -mglob -t"Your familiar's name is now Vampire." name_korfula = /repeat -5 1 /eval !do order vampire name Korfula;order vampire follow;order vampire setwimpy 30;order vampire wear all;order vampire wield all
-/def -p1 -mglob -t"Your familiar's name is now Mummy." name_tp = /repeat -5 1 /eval !do order mummy name Tp;order mummy follow;order mummy setwimpy 30;order mummy wear all
+/def -p1 -mglob -t"Your familiar's name is now Wraith." name_wraith = /repeat -5 1 /eval !do order wraith name %{wraith_name};order wraith follow;order wraith setwimpy 30;order wraith wear all;order wraith wield all
+/def -p1 -mglob -t"Your familiar's name is now Vampire." name_vampire = /repeat -5 1 /eval !do order vampire name %{vampire_name};order vampire follow;order vampire setwimpy 30;order vampire wear all;order vampire wield all
+/def -p1 -mglob -t"Your familiar's name is now Mummy." name_mummy = /repeat -5 1 /eval !do order mummy name %{mummy_name};order mummy follow;order mummy setwimpy 30;order mummy wear all
+
 ;Blasts
 /def -p1 -mglob -t"You mutter the words 'morrrrm o'r aa'hgda'" re_blist = !cast blister skin
 /def -p1 -mglob -t"You concentrate deeply before unleashing a blast of psionic energy at *." re_mblast = !cast mind blast
 /def -p1 -mglob -t"You draw symbols of disease on * body!" rott_limb = !cast rotting limbs
 /def -p1 -mglob -t"You mutter the words 'fhjja'as kc'tta'" scc = !cast summon carrion crawlers
 /def -p1 -mglob -t"Fistandantilus mutters the words 'buuunes 'ot 'ot Flames'" re_bbones = !order fistandantilus cast burning bones
-/def -p1 -mglob -t"Nessy is ready to perform its skill." re_bash = order nessy use bash
-/def -p1 -mglob -t"LordSoth mutters the words 'Ogmis Yugu Oy'wercs!'" re_eth_fist = order LordSoth cast ethereal fist
-/def -p1 -mglob -t"Wilbur mutters the words 'Ogmis Yugu Oy'wercs!'" re_ethfist = !order wilbur cast ethereal fist
-/def -p1 -mglob -t"Rott tries to infect *, but fails miserably!" re_ench = !order %{zombie_name} use encephalitis
-/def -p1 -mglob -t"Wilbur mutters the words 'Whooooomp SPLAT!'" re_intrude = !order wilbur cast mind intrusion
-/def -p1 -mglob -t"Tp focuses inwards to connect with his ka, but fails miserably." re_akh_ba = !order tp use akh ba attack
-/def -p1 -mglob -t"Tp focuses inwards and connects with his ka. *" re_akh_ba2 = !order tp use akh ba attack
-/def -p1 -mglob -t"Korfula is * as his skill *" re_kol = !order korfula use kiss of life
-/def -p1 -mglob -t"Wilbur mutters the words 'kbnaaaang kbNAAANG!'" re_eblast = !order wilbur cast ethereal blast
-/def -p1 -mglob -t'Rott infects * with encephalitis.' do_decay = !order %{zombie_name} cast touch of decay
-/def -p1 -mglob -t"Wilbur mutters the words 'SPAL inf flff luff!'" do_invade = !order wilbur cast mind invasion
+; /def -p1 -mglob -t"LordSoth mutters the words 'Ogmis Yugu Oy'wercs!'" re_eth_fist = order LordSoth cast ethereal fist
+/eval /def -p1 -mglob -t"%{abom_name} is ready to perform its skill." re_bash = order %{abom_name} use bash
+/eval /def -p1 -mglob -t"%{mummy_name} focuses inwards to connect with his ka, but fails miserably." re_akh_ba = !order %{mummy_name} use akh ba attack
+/eval /def -p1 -mglob -t"%{mummy_name} focuses inwards and connects with his ka. *" re_akh_ba2 = !order %{mummy_name} use akh ba attack
+; Recast kiss of life. Commented out because the skill has a cooldown and can't be instantly recast
+; TODO Figure out how long before recast, and set a timer. Then break that timer when a mob dies
+; /eval /def -p1 -mglob -t"%{vampire_name} sinks his fangs deep into * neck, biting and sucking out * life blood." re_kol = !order %{vampire_name} use kiss of life
+; To recast when vmap fails kiss of life
+/eval /def -p1 -mglob -t"%{vampire_name} is * as his skill *" re_kol = !order %{vampire_name} use kiss of life
+/eval /def -p1 -mglob -t"%{wraith_name} mutters the words 'Ogmis Yugu Oy'wercs!'" re_ethfist = !order %{wraith_name} cast ethereal fist
+/eval /def -p1 -mglob -t"%{wraith_name} mutters the words 'SPAL inf flff luff!'" do_invade = !order %{wraith_name} cast mind invasion
+/eval /def -p1 -mglob -t"%{wraith_name} mutters the words 'kbnaaaang kbNAAANG!'" re_eblast = !order %{wraith_name} cast ethereal blast
+/eval /def -p1 -mglob -t"%{wraith_name} mutters the words 'Whooooomp SPLAT!'" re_intrude = !order %{wraith_name} cast mind intrusion
+/eval /def -p1 -mglob -t"%{zombie_name} tries to infect *, but fails miserably!" re_ench = !order %{zombie_name} use encephalitis
+; /eval /def -p1 -mglob -t'%{zombie_name} infects * with encephalitis.' do_decay = !order %{zombie_name} cast touch of decay
+; /eval /def -p1 -mglob -t'%{zombie_name} infects * with encephalitis.' do_anemia = !order %{zombie_name} use anemia
 
 ;Prots
 /def -p1 -mglob -t"You are surrounded by an energy field." efield_up = /status_edit Efield:6:BCGreen
@@ -61,10 +78,10 @@
 /def -p1 -mglob -t'You close your eyes, then open filled with the spirit of the rogue.' swr_up = /status_edit Swr:5:BCGreen
 /def -p1 -mglob -t'You lose your contact with the dead rogue.' srw_down = /status_edit Swr:5:BCRed
 
-/def -p1 -mglob -t'Nessy focuses its will and forms a powerful shield around itself!' und_shield_up = \
-!%{prot_chan} \%^BOLD\%^CYAN\%^Undying Shield \%^RED\%^\[\%^GREEN\%^Up\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^Nessy\%^RESET\%^
-/def -p1 -mglob -t'Nessy loses its concentration and the shield around it disappears.' und_shield_down = \
-!%{prot_chan} \%^BOLD\%^CYAN\%^Undying Shield \%^RED\%^\[\%^BLUE\%^Down\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^Nessy\%^RESET\%^
+/eval /def -p1 -mglob -t'%{abom_name} focuses its will and forms a powerful shield around itself!' und_shield_up = \
+!%{prot_chan} \%^BOLD\%^CYAN\%^Undying Shield \%^RED\%^\[\%^GREEN\%^Up\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^%{abom_name}\%^RESET\%^
+/eval /def -p1 -mglob -t'%{abom_name} loses its concentration and the shield around it disappears.' und_shield_down = \
+!%{prot_chan} \%^BOLD\%^CYAN\%^Undying Shield \%^RED\%^\[\%^BLUE\%^Down\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^%{abom_name}\%^RESET\%^
 
 ;Debuffs
 /def -p1 -mregexp -t'^([A-Z][a-z]+) becomes more susceptible to diseases.' vc_up = \
@@ -81,14 +98,14 @@
 !say \%^BOLD\%^CYAN\%^Rotting Limbs \%^RED\%^\[\%^BLUE\%^Down\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^%P1\%^RESET\%^
 
 ;Scourge buffs
-/def -p1 -mglob -t"Nessy becomes more resistant." parenga_up = \
-!%{prot_chan} \%^BOLD\%^CYAN\%^Parenga \%^RED\%^\[\%^GREEN\%^Up\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^Nessy\%^RESET\%^
-/def -p1 -mglob -t"Nessy becomes less resistant." parenga_down = \
-!%{prot_chan} \%^BOLD\%^CYAN\%^Parenga \%^RED\%^\[\%^BLUE\%^Down\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^Nessy\%^RESET\%^
-/def -p1 -mglob -t"Wilbur grows smarter." roro_up = \
-!%{prot_chan} \%^BOLD\%^CYAN\%^Roro \%^RED\%^\[\%^GREEN\%^Up\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^Wilbur\%^RESET\%^
-/def -p1 -mglob -t"Wilbur becomes dumber." roro_down = \
-!%{prot_chan} \%^BOLD\%^CYAN\%^Roro \%^RED\%^\[\%^BLUE\%^Down\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^Wilbur\%^RESET\%^
+/eval /def -p1 -mglob -t"%{abom_name} becomes more resistant." parenga_up = \
+!%{prot_chan} \%^BOLD\%^CYAN\%^Parenga \%^RED\%^\[\%^GREEN\%^Up\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^%{abom_name}\%^RESET\%^
+/eval /def -p1 -mglob -t"%{abom_name} becomes less resistant." parenga_down = \
+!%{prot_chan} \%^BOLD\%^CYAN\%^Parenga \%^RED\%^\[\%^BLUE\%^Down\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^%{abom_name}\%^RESET\%^
+/eval /def -p1 -mglob -t"%{wraith_name} grows smarter." roro_up = \
+!%{prot_chan} \%^BOLD\%^CYAN\%^Roro \%^RED\%^\[\%^GREEN\%^Up\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^%{wraith_name}\%^RESET\%^
+/eval /def -p1 -mglob -t"%{wraith_name} becomes dumber." roro_down = \
+!%{prot_chan} \%^BOLD\%^CYAN\%^Roro \%^RED\%^\[\%^BLUE\%^Down\%^RED\%^\]\%^CYAN\%^ on \%^RESET\%^BOLD\%^%{wraith_name}\%^RESET\%^
 
 ;Mindsurge
 /def -p1 -mregexp -t"You focus inwards and suddenly a flow of energy passes from you, through your wooden staff of the necromancer into ([A-Z][a-z]+)!" surge_rep = !order %P1 report
@@ -115,5 +132,5 @@
 /status_add -Amy_hunger -s1 -x mon_health:3:BCRed
 
 ;mob highlights
-/def -aCred -p1 -mregexp -t'(Rott||Wilbur) begins to chant.'
-/def -aCred -p1 -mregexp -t'(Rott|Nessy|Tp|Korfula) prepares (him|it)self to use a skill.'
+/eval /def -aCred -p1 -mregexp -t'^(%{zombie_name}|%{wraith_name}) begins to chant.'
+/eval /def -aCred -p1 -mregexp -t'(%{zombie_name}|%{abom_name}|%{mummy_name}|%{vampire_name}) prepares (him|it)self to use a skill.'
