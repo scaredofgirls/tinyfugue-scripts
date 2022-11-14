7f141ce4cd93389584399c4bd1bd12301d5a865d
Kap <github@scaredofgirls.net>
2022-11-13 23:19:40 -0500

    Adding iom.herbalist.tf

---
+++ iom.herbalist.tf
@@ -0,0 +1,10 @@
+; Balms
+/def -mglob -p1 -t'Your skin shimmers a while and seems to be hardened.' protbalm_up = !%{prot_chan} \%^BOLD\%^CYAN\%^Protection Balm \%^RED\%^\[\%^GREEN\%^Up\%^RED\%^\]\%^RESET\%^%;
+/def -mglob -p1 -t'Your skin seems to be soft again.' protbalm_down = !%{prot_chan} \%^BOLD\%^CYAN\%^Protection Balm \%^RED\%^\[\%^BLUE\%^Down\%^RED\%^\]\%^RESET\%^%;
+
+; Potions
+/def -mglob -p1 -t'You feel a lot stronger and stouter!' warpot_up = !%{prot_chan} \%^BOLD\%^CYAN\%^Warrior Potion \%^RED\%^\[\%^GREEN\%^Up\%^RED\%^\]\%^RESET\%^%;
+/def -mglob -p1 -t"You feel weaker as the potion's magic leaves you." warpot_down = !%{prot_chan} \%^BOLD\%^CYAN\%^Warrior Potion \%^RED\%^\[\%^BLUE\%^Down\%^RED\%^\]\%^RESET\%^%;
+;rogue
+;You feel slower as the potion effect fades.
+
